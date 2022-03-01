451414170efc13ab4a932a23642dc0ccb8c03e33
sebyyyy <94870277+EmaHermankova@users.noreply.github.com>
2022-03-01 09:08:59 +0100

    Add files via upload

---
+++ Idk.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package idk;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 2021-e-hermankova
+ */
+public class Idk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr;
+        arr = new int[13];
+        for(int y=0; y<13; y++){
+            arr[y]=scanner.nextInt();
+            
+        }
+        
+            
+        
+    }
+    
+}
